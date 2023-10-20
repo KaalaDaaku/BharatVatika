803dc3d612a0bbac1e8516db26a4ab984be6e3c9
Sumeet-Parab-2003 <sumeetparab4113@gmail.com>
2023-10-20 13:18:35 +0530

    final

--- src/layouts/home/Home.layout.jsx
+++ src/layouts/home/Home.layout.jsx
@@ -15,7 +15,7 @@ const Home = () => {
       {/* <CatagorySection/> */}
       <AboutSection />
 
-      <YoutubeSection />
+      {/* <YoutubeSection /> */}
       <BlogSection />
     </Fragment>
   );
