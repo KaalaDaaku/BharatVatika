43589fb7dfcaa20ccd419d601734c9d3dd05c064
Sumeet-Parab-2003 <sumeetparab4113@gmail.com>
2023-10-31 12:47:41 +0530

    done

---
+++ src/components/contact-section/ContactSection.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const ContactSection = () => {
+  return (
+    <div className='p-5'>
+    <div className='row'>
+    <div className='mb-4'>
+            <h1>Contact Us</h1>
+        </div>
+        <div className='mb-5'>
+        <p><i className="fas fa-home me-3"></i> LTCE, Navi Mumbai</p>
+            <p>
+                <i className="fas fa-envelope me-3"></i>
+                sumeetparab_ce_2021@ltce.in
+            </p>
+            <p><i className="fas fa-phone me-3"></i> + 91 87795 36101</p>
+            
+        </div>
+        
+    </div>
+    <div className='row '>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.0443640152275!2d73.0072481!3d19.1057096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c0da59d0a01b%3A0x56642a54a185f894!2sLokmanya%20Tilak%20College%20of%20Engineering!5e0!3m2!1sen!2sin!4v1698735713489!5m2!1sen!2sin" width="400" height="300" allowfullscreen="false" loading="lazy" ></iframe>    </div>
+    </div>
+  )
+}
+
+export default ContactSection
